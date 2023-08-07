b3d855ed76f93e19028a993da81e97b343b8aa1a
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-08-07 23:59:19 +0300

    feat: create posts section

---
+++ components/posts.tsx
@@ -0,0 +1,10 @@
+import styles from "./posts.module.css";
+
+export function Posts() {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.name}>Posts</h2>
+      <p className={styles.description}>Nothing yet here.</p>
+    </section>
+  );
+}
