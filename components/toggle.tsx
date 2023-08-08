9be1156998ca5d47276effafd303a7df3198d85e
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-08-08 03:14:31 +0300

    fix: add different icon for dark and fix not mounded theme problem.

--- components/toggle.tsx
+++ components/toggle.tsx
@@ -1,17 +1,27 @@
 "use client";
 import { useTheme } from "next-themes";
-import { SunIcon } from "@radix-ui/react-icons";
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
 import styles from "./toggle.module.css";
+import * as React from "react";
 
 export function Toggle() {
+  const [mounted, setMounted] = React.useState(false);
   const { theme, setTheme } = useTheme();
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null; //TODO: add loading spinner
+  }
   return (
     <button
       type="button"
       className={styles.toggle}
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      <SunIcon />
+      {theme === "light" ? <SunIcon /> : <MoonIcon />}
     </button>
   );
 }
