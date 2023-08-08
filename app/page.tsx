7bb66319183dd543a8289cf5749d7967c9659d48
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-08-08 17:17:06 +0300

    feat: add projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Posts } from "@/components/posts";
+import { Projects } from "@/components/projects";
 
 export default function Home() {
   return (
     <>
       <Posts />
+      <Projects />
     </>
   );
 }
