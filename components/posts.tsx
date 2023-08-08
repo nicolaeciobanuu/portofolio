41a2875230351481ccd35bd08892fce2d3fdad5e
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-08-08 17:18:16 +0300

    chore: change h2 to h1

--- components/posts.tsx
+++ components/posts.tsx
@@ -3,7 +3,7 @@ import styles from "./posts.module.css";
 export function Posts() {
   return (
     <section className={styles.container}>
-      <h2 className={styles.name}>Posts</h2>
+      <h1 className={styles.name}>Posts</h1>
       <p className={styles.description}>Nothing yet here.</p>
     </section>
   );
