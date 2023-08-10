c604cc4c6b98e2ab6a9cd63eaede7b87c0bfbed9
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-08-10 18:56:00 +0300

    feat: add SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,36 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { ThemeProvider } from "./theme-provider";
 import { Header } from "@/components/header";
+import { url } from "inspector";
 export const metadata: Metadata = {
   title: "Ciobanu Nicolae",
   description: "Portofolio of Ciobanu Nicolae",
+  keywords: [
+    "Ciobanu",
+    "Nicolae",
+    "Portofolio",
+    "Next.js",
+    "React",
+    "TypeScript",
+    "Radix UI",
+    "Vercel",
+    "GitHub",
+  ],
+  authors: [
+    { name: "Ciobanu Nicolae", url: "https://github.com/ciobanunicolae" },
+  ],
+  creator: "ciobanunicolae",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    url: "https://ciobanunicolae.com",
+    title: "Ciobanu Nicolae",
+    description: "Portofolio of Ciobanu Nicolae",
+    siteName: "Ciobanu Nicolae",
+  },
 };
 
 export default function RootLayout({
