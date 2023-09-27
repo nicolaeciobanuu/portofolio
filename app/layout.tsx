9ec2e073de455400858ec23b04b8803e1a9ae79f
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-27 12:31:07 +0300

    feat: add favicons for both themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import "./styles/_globals.scss";
+import "./globals.css";
 import type { Metadata } from "next";
 import { Providers } from "./providers";
-import { Header } from "@/components/header";
+import { Header, Footer } from "@/components";
+
 export const metadata: Metadata = {
   title: "Ciobanu Nicolae",
   description: "Portofolio of Ciobanu Nicolae",
@@ -31,6 +32,20 @@ export const metadata: Metadata = {
     description: "Portofolio of Ciobanu Nicolae",
     siteName: "Ciobanu Nicolae",
   },
+  icons: {
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+    icon: [
+      {
+        url: "/favicon-light.ico",
+        media: "(prefers-color-scheme: light)",
+      },
+      {
+        url: "/favicon-dark.ico",
+        media: "(prefers-color-scheme: dark)",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -43,7 +58,8 @@ export default function RootLayout({
       <body>
         <Providers defaultTheme="system" enableSystem>
           <Header />
-          {children}
+          <main>{children}</main>
+          <Footer />
         </Providers>
       </body>
     </html>
