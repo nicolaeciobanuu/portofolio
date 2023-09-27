a2752cb715db5e7584f0a4fd468a3f241d010660
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-27 12:27:59 +0300

    refactor: add config file

---
+++ config/index.ts
@@ -0,0 +1,17 @@
+export const ProjectsList = [
+  {
+    title: "huez",
+    description: "Automatic color palette generator",
+    link: "https://huez.ciobanunicolae.com",
+  },
+  {
+    title: "pomodoro",
+    description: "Configurable productivity timer",
+    link: "https://pomodoro.ciobanunicolae.com",
+  },
+  {
+    title: "shortest-path-vis",
+    description: "Shortest path between two points",
+    link: "https://github.com/ciobanunicolae/shortest-path-vis",
+  },
+];
