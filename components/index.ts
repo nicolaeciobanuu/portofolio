82f756780ac7f4b791712a8b05446daf8e13601e
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2023-09-27 12:29:45 +0300

    feat: create reexport file

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+export * from "./card/card";
+export * from "./header/header";
+export * from "./posts/posts";
+export * from "./projects/projects";
+export * from "./toggle/toggle";
+export * from "./separator/separator";
+export * from "./footer/footer";
